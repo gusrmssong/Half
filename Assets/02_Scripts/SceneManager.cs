2f06e28ce2d35097d493c91e05ebec9874e8a631
gusrms405 <gusrms4051@gmail.com>
2026-02-02 10:44:20 +0900

    0202Morning

---
+++ Assets/02_Scripts/SceneManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HalfSceneManager : MonoBehaviour
+{
+    public static HalfSceneManager Instance;
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneMain();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneGameReady();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SceneGame();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SceneShop();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            SceneEnding();
+        }
+    }
+    
+    public void SceneMain()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void SceneGameReady()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void SceneGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void SceneShop()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void SceneEnding()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+
+
+}
