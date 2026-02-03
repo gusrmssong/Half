11405b6a3c7d4819263eea99b88236e81aa56b02
gusrms405 <gusrms4051@gmail.com>
2026-02-03 11:47:52 +0900

    0203 1200

--- Assets/02_Scripts/02_GameObject/Cell.cs
+++ Assets/02_Scripts/02_GameObject/Cell.cs
@@ -2,45 +2,98 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.UI.CanvasScaler;
 
 public class Cell : MonoBehaviour
 {
     [SerializeField] public int x;
     [SerializeField] public int y;
 
-    [SerializeField] public bool isObstacle;
-    [SerializeField] public bool isRevealed;
+    [SerializeField] public bool isObstacle = false;
+    [SerializeField] public bool isRevealed = false;
+    [SerializeField] public bool isDestroyed = false;
     [SerializeField] public Unit unit;
 
-    public void Init(int x, int y, bool isRevealed, Unit unit)
+    [SerializeField] public Image unitSprite;
+    [SerializeField] public Image destroySprite;
+    [SerializeField] public Image revealSprite;
+
+    [SerializeField] public Sprite[] unitSprites;
+    public void Init(int x, int y, Unit unit)
     {
         this.x = x;
         this.y = y;
-        this.isRevealed = isRevealed;
         this.unit = unit;
         gameObject.name = $"Cell_{x}_{y}";
+        UpdateCellSprite();
     }
 
-    public void UpdateCell()
+    private void Awake()
     {
-        if(isRevealed)
-        {
+        unitSprite = GetComponent<Image>();
+    }
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            Reveal();
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Destory();
+        }
+        if (Input.GetKey(KeyCode.C))
+        {
+            if(Input.GetKeyDown(KeyCode.A))
+            {
+                SetUnit(0);
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                SetUnit(1);
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                SetUnit(2);
+            }
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                SetUnit(3);
+            }
+        }
+    }
+
+    public void Reveal()
+    {
+        isRevealed = !isRevealed;
+        UpdateCellSprite();
+    }
+    public void Destory()
+    {
+        isDestroyed = !isDestroyed;
+        UpdateCellSprite();
+    }
+    public void SetUnit(int input)
+    {
+        unit = (Unit)input;
+        UpdateCellSprite();
+    }
+
+    public void UpdateCellSprite()
+    {
+        int num = (int)unit;
+        if (num >= 0 && num < unitSprites.Length)
+        {
+            unitSprite.sprite = unitSprites[num];
         }
         else
         {
-
+            Debug.Log("해당하는 유닛 스프라이트가 없습니다");
         }
-
-        if(unit!=Unit.None)
-        {
-
-        }
-        else
-        {
-
-        }
-
+        revealSprite.gameObject.SetActive(!isRevealed);
+        destroySprite.gameObject.SetActive(isDestroyed);
     }
 
 }
